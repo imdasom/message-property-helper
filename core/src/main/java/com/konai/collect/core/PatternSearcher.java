3e21aea70d3e7000c8a4c3fbfd74c3d73dbf1878
imdasom <imdasom101@gmail.com>
2018-11-04 16:36:31 +0900

    프로젝트 구조 수정 (multi module project)

--- core/src/main/java/com/konai/collect/core/PatternSearcher.java
+++
@@ -1,8 +0,0 @@
-package com.konai.collect.core;
-
-import java.util.List;
-
-public interface PatternSearcher<T, U> {
-
-    List<U> get(T source);
-}
--- gui/src/main/java/properties/messages/gui/application/MainPanel.java
+++
@@ -1,7 +0,0 @@
-package properties.messages.gui.application;
-
-import java.io.Serializable;
-
-public class MainPanel implements Serializable {
-
-}
